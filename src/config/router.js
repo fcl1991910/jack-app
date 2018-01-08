84cda841cf205cce724d24ebe2e40d6e77cdfcc7
chenglin fan <chenglinfan@chenglins-MacBook-Pro.local>
2018-01-08 23:26:21 +1100

    sas

--- src/config/router.js
+++ src/config/router.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { TabNavigator } from 'react-navigation';
-import {Icon} from 'react-native-elements';
+import React from "react";
+import { TabNavigator,StackNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
 
-import Screen1 from '../screens/Screen1';
-import Screen2 from '../screens/Screen2';
+import Screen1 from "../screens/Screen1";
+import Screen2 from "../screens/Screen2";
+import Screen3 from "../screens/Screen3";
 
 export const Tabs = TabNavigator({
   Screen1: {
     screen: Screen1,
+    navigationOptions: {
+        tabBarLabel: "Products",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="list" size={35} color={tintColor} />
+        )
+    }
   },
   Screen2: {
     screen: Screen2,
-  },
+    navigationOptions: {
+      tabBarLabel: "Jack",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="account-circle" size={35} color={tintColor} />
+        )
+    }
+  }
 });
---
+++ src/screens/Screen3.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {StyleSheet, Text, View} from 'react-native';
+
+const Screen3 = () => (
+    <View style={styles.container}>
+    <Text>3333333333333333</Text>
+</View>
+);
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  });
+
+export default Screen3;
